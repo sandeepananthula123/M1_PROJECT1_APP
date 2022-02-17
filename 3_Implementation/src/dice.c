4ca86c40f2b7342b342f194d2b1731370ef451d1
sandeepananthula123 <71876688+sandeepananthula123@users.noreply.github.com>
2022-02-17 23:15:27 +0530

    Update dice.c

--- 3_Implementation/src/dice.c
+++ 3_Implementation/src/dice.c
@@ -1,1 +1,13 @@
-
+#include"dice.h"
+#include<time.h>
+#include<stdlib.h>
+int dice_output()
+{
+    int upper = 6, lower = 1, dice;
+    srand (time (0));
+    A:dice = (rand () % (upper - lower + 1)) + lower;
+    if (dice == 0)
+      goto A;
+    else
+      return dice;
+}
