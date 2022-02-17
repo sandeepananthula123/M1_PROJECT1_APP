4e8e36aa2fc0a4f20118613fc94e5e8a077ee01d
sandeepananthula123 <71876688+sandeepananthula123@users.noreply.github.com>
2022-02-17 23:14:46 +0530

    Create dice1.c

---
+++ 3_Implementation/src/dice1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include"dice.h"
+char *dice1()
+{
+	char print[100]="Roll=6: You have earned a chance to play one more time.";
+	
+	printf("%s\n\n",print);
+	char *cp=print;
+	return cp;
+}
