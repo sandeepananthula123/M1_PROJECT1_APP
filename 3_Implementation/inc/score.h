3107256ffc9898f38c1565c35483b94755ae4a1d
sandeepananthula123 <71876688+sandeepananthula123@users.noreply.github.com>
2022-02-17 23:12:47 +0530

    Update score.h

--- 3_Implementation/inc/score.h
+++ 3_Implementation/inc/score.h
@@ -1,1 +1,15 @@
-
+#ifndef _DICE_H_
+#define _DICE_H_
+/**
+ * @brief Function to find the output of a dice roll
+ * @return int result of dice_output operation
+ * 
+ */
+int dice_output();
+/**
+ * @brief Function to check if output of dice is 6
+ * @param[in] diceout Number whose value is to be checked for 6
+ * @return int result after checking
+ */
+char *diceout6();
+#endif/*_DICE_H_*/
